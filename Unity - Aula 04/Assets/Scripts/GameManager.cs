454cada7489046dd31c3692f2be174fb121d5c58
EvertonHimself <evertondeoliveira@gmail.com>
2019-10-29 15:04:06 -0300

    Create the BoardManager script.
    Created the board manager script that will instantiate the parts of the level as well as the enemies and itens.

---
+++ Unity - Aula 04/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public BoardManager boardScript;
+
+    private int level = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
